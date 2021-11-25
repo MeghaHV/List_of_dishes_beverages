ea759a0dde9b1eb0711df7972c0f974c8fad2beb
MeghaHV <meghahv16@gmail.com>
2021-11-25 19:57:47 +0530

    final commit

--- app/src/main/java/com/example/listofdishes/data/Datasource.kt
+++ app/src/main/java/com/example/listofdishes/data/Datasource.kt
@@ -8,16 +8,16 @@ class Datasource() {
 
     fun loadListOfDishesBeverages(): List<ListOfDishesBeverages> {
         return listOf<ListOfDishesBeverages>(
-            ListOfDishesBeverages(R.string.title1, R.drawable.image01,R.string.ingredients,R.string.description,R.drawable.image01),
-            ListOfDishesBeverages(R.string.title2, R.drawable.image02,R.string.ingredients1,R.string.description1,R.drawable.image02),
-            ListOfDishesBeverages(R.string.title3, R.drawable.image03,R.string.ingredients,R.string.description,R.drawable.image03),
-            ListOfDishesBeverages(R.string.title4, R.drawable.image04,R.string.ingredients,R.string.description,R.drawable.image04),
-            ListOfDishesBeverages(R.string.title5, R.drawable.image05,R.string.ingredients,R.string.description,R.drawable.image05),
-            ListOfDishesBeverages(R.string.title6, R.drawable.image06,R.string.ingredients,R.string.description,R.drawable.image06),
-            ListOfDishesBeverages(R.string.title7, R.drawable.image07,R.string.ingredients,R.string.description,R.drawable.image07),
-            ListOfDishesBeverages(R.string.title8, R.drawable.image08,R.string.ingredients,R.string.description,R.drawable.image08),
-            ListOfDishesBeverages(R.string.title9, R.drawable.image09,R.string.ingredients,R.string.description,R.drawable.image09),
-            ListOfDishesBeverages(R.string.title10, R.drawable.image010,R.string.ingredients,R.string.description,R.drawable.image010)
+            ListOfDishesBeverages(R.string.title1, R.drawable.image01,R.string.ingredients1,R.string.description1,R.drawable.image01),
+            ListOfDishesBeverages(R.string.title2, R.drawable.image02,R.string.ingredients2,R.string.description2,R.drawable.image02),
+            ListOfDishesBeverages(R.string.title3, R.drawable.image03,R.string.ingredients3,R.string.description3,R.drawable.image03),
+            ListOfDishesBeverages(R.string.title4, R.drawable.image04,R.string.ingredients4,R.string.description4,R.drawable.image04),
+            ListOfDishesBeverages(R.string.title5, R.drawable.image05,R.string.ingredients5,R.string.description5,R.drawable.image05),
+            ListOfDishesBeverages(R.string.title6, R.drawable.image06,R.string.ingredients6,R.string.description6,R.drawable.image06),
+            ListOfDishesBeverages(R.string.title7, R.drawable.image07,R.string.ingredients7,R.string.description7,R.drawable.image07),
+            ListOfDishesBeverages(R.string.title8, R.drawable.image08,R.string.ingredients8,R.string.description8,R.drawable.image08),
+            ListOfDishesBeverages(R.string.title9, R.drawable.image09,R.string.ingredients9,R.string.description9,R.drawable.image09),
+            ListOfDishesBeverages(R.string.title10, R.drawable.image010,R.string.ingredients10,R.string.description10,R.drawable.image010)
         )
     }
 }
